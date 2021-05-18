586acb57e8fbd83f0366082483fb3ec402dfdbbc
GP <gregpetropoulos@yahoo.com>
2021-05-18 06:58:32 -0400

    deploying

--- server.js
+++ server.js
@@ -19,7 +19,9 @@ app.use(express.static("public"));
 
 // https://developer.mozilla.org/en-US/docs/Learn/Server-side/Express_Nodejs/mongoose
 // SET UP MONGOOSE CONNECT
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
+
+//mongodb+srv://gregadmindb:n-i#yWTjg35._E!@cluster0.qste2.mongodb.net/Workout?retryWrites=true&w=majority
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/Workout", {
   useNewUrlParser: true,
   useCreateIndex: true,
   useUnifiedTopology: true,
